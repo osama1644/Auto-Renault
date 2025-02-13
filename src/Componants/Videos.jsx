77516b70b745d8be0b2ec7910a001f171a23c85e
osama1644 <latifosama605@gmail.com>
2025-02-13 19:31:26 +0200

    تعديل مسارات الفيديوهات

--- src/Componants/Videos.jsx
+++ src/Componants/Videos.jsx
@@ -7,15 +7,16 @@ function Videos() {
       <Navbar />
       <div className="videos">
         <div className="container">
-          <video controls>
-            <source src="/public/WhatsApp Video 2025-02-06 at 21.06.21_7512e34c.mp4" type="video/mp4" />
-          </video>
-          <video controls>
-            <source src="/public/WhatsApp Video 2025-02-06 at 21.06.45_96fbe17a.mp4" type="video/mp4" />
-          </video>
-          <video controls>
-            <source src="/public/WhatsApp Video 2025-02-06 at 21.07.15_cbfe8da2.mp4" type="video/mp4" />
-          </video>
+        <video controls>
+  <source src="/WhatsApp Video 2025-02-06 at 21.06.21_7512e34c.mp4" type="video/mp4" />
+</video>
+<video controls>
+  <source src="/WhatsApp Video 2025-02-06 at 21.06.45_96fbe17a.mp4" type="video/mp4" />
+</video>
+<video controls>
+  <source src="/WhatsApp Video 2025-02-06 at 21.07.15_cbfe8da2.mp4" type="video/mp4" />
+</video>
+
         </div>
       </div>
     </>
